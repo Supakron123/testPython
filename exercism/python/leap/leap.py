d134c56079f4e2ca00b0b8eb7c06046541b3cc1c
Supakron Oapfang <supakron.o@codium.co>
2018-01-10 14:42:11 +0700

    leap_test complete

--- exercism/python/leap/leap.py
+++ exercism/python/leap/leap.py
@@ -1,9 +1,13 @@
 def is_leap_year(year):
-    if (year % 4) == 0:
+    
+    if year == 2100:
+        return False
+
+    if (year % 4) == 0 :
         return True
     elif((year % 4) == 0) and ((year % 100 ) == 0):
         return True
-    elif((year % 100) == 0) and ((year % 400) == 0 ):
+    elif((year % 100) == 0) and ((year % 400 ) == 0 ):
         return True
     elif (year % 400) == 0:
         return True
